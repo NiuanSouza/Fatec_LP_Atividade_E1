210b00cd1a7725099b415c4ddb0ebca4b82a8232
NiuanSouza <niuan.spolid@hotmail.com>
2025-09-22 22:43:27 -0300

    addMusicInterface

---
+++ Programa/src/main/java/com/example/tgcontrol/MusicClass/Valour.java
@@ -0,0 +1,19 @@
+package com.example.tgcontrol.MusicClass;
+
+public class Valour {
+    private String emotion;
+    private String action;
+    private String destination;
+    private String valourReturn;
+
+    public Valour(String emotion, String action, String destination) {
+        this.emotion = emotion;
+        this.action = action;
+        this.destination = destination;
+    }
+
+    public String liftSouls() {
+        valourReturn = ("In " + emotion + ", we " + action + " our souls to " + destination + ".");
+        return valourReturn;
+    }
+}
